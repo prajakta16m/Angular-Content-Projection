5ebfde1c483e1bcab13a9e22e413ab308e82d465
prajakta16m <prajakta.mahamuni91@gmail.com>
2024-08-10 14:18:08 +0530

    multi slot content projection

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { SingleSlotComponent } from './single-slot/single-slot.component';
+import { MultiSlotComponent } from './multi-slot/multi-slot.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SingleSlotComponent],
+  imports: [SingleSlotComponent, MultiSlotComponent],
   template: `
     <h1>Let's learn angular's content projection</h1>
     <a target="_blank" href="https://angular.dev/overview">
@@ -14,6 +15,10 @@ import { SingleSlotComponent } from './single-slot/single-slot.component';
     <app-single-slot>
       This is the single slot content being projected from the single slot component !
     </app-single-slot>
+    <app-multi-slot>
+      <div header>This is the header</div>
+      <div body>Lorem ipsum de color blue</div>
+    </app-multi-slot>
   `,
 })
 export class App {
